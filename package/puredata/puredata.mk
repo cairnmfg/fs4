297af7d0d0c5c8791861b1fba8d05d07f402dc89
Dylan Fareed <email@dylanfareed.com>
2022-06-21 10:58:22 -0400

    Revert puredata package files

--- package/puredata/puredata.mk
+++ package/puredata/puredata.mk
@@ -4,32 +4,19 @@
 #
 ################################################################################
 
-PUREDATA_VERSION = 0.49-0
-PUREDATA_SITE = https://github.com/pure-data/pure-data/archive
-PUREDATA_SOURCE = $(PUREDATA_VERSION).tar.gz
-PUREDATA_LICENSE = BSD-3-Clause
-PUREDATA_LICENSE_FILES = LICENSE.txt
-PUREDATA_DEPENDENCIES = alsa-lib
+PUREDATA_VERSION = 0.46-7
+PUREDATA_SOURCE = pd-$(PUREDATA_VERSION).armv7.tar.gz
+PUREDATA_SITE = http://msp.ucsd.edu/Software
+PUREDATA_INSTALL_STAGING = YES
+PUREDATA_INSTALL_TARGET = YES
 PUREDATA_AUTORECONF = YES
+PUREDATA_CONF_OPTS = --disable-portaudio --disable-portmidi --no-recursion
+PUREDATA_DEPENDENCIES = alsa-utils gettext tcl
 
-# Fix for toolchains using the musl libc
-ifeq ($(BR2_TOOLCHAIN_USES_MUSL),y)
-PUREDATA_CONF_OPTS += CFLAGS="$(TARGET_CFLAGS) -D__off64_t=off64_t"
-endif
-
-# Portaudio is for Mac/Windows cross compatibility, we don't need it
-PUREDATA_CONF_OPTS += --disable-portaudio
-
-# Create the empty /m4/generated directory
-# (In a normal puredata build, this would be done by autogen.sh)
-define PUREDATA_POST_EXTRACT_FIXUP
- mkdir -p $(@D)/m4/generated
+define PUREDATA_RUN_AUTOGEN
+    cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
 endef
-PUREDATA_POST_EXTRACT_HOOKS += PUREDATA_POST_EXTRACT_FIXUP
 
-ifeq ($(BR2_PACKAGE_PUREDATA_JACK),y)
-PUREDATA_DEPENDENCIES += jack2
-PUREDATA_CONF_OPTS += --enable-jack
-endif
+PUREDATA_PRE_CONFIGURE_HOOKS += PUREDATA_RUN_AUTOGEN
 
 $(eval $(autotools-package))
